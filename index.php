87f3ee6d387e7b137a86d30f992f44b737bc8fbf
herr-hxp <haxxpet@gmail.com>
2023-08-07 15:03:49 +0200

    liven up the index

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-  echo gethostname();
-?>
+<html>
+  <head>
+  </head>
+  <body>
+    <img src="img/elastx_logo_light.png" />
+      <p>Served by: 
+        <?php
+          echo gethostname();
+        ?>
+    </p>
+  </body>
+</html>
