2363869a0b665e22ec59a91cde96ab54d9912d53
herr-hxp <haxxpet@gmail.com>
2023-08-17 11:05:29 +0200

    removed the seconds from the date output

--- scripts/modify-index.sh
+++ scripts/modify-index.sh
@@ -4,5 +4,5 @@ INDEX_FILE='index.php'
 BACKUP_FILE='bak.index.php'
 
 [[ ! -e ${BACKUP_FILE} ]] && cp ${INDEX_FILE} ${BACKUP_FILE}
-sed -i '/echo.*/a echo "<br />" . date("Y-m-d H:i:s");' ${INDEX_FILE}
+sed -i '/echo.*/a echo "<br />" . date("Y-m-d H:i");' ${INDEX_FILE}
 
