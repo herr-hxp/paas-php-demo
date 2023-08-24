a29a734e1e8a25b99850132f4f0d5a48f658fc2d
herr-hxp <haxxpet@gmail.com>
2023-08-24 10:29:53 +0200

    WE FIX

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php ekajeka ?>
+<html>
+  <head>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <img src="img/elastx_logo_light.png" />
+      <p><b>Served by:</b> 
+        <?php
+          echo gethostname();
+        ?>
+    </p>
+  </body>
+</html>
