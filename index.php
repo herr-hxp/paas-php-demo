4fc26f5587b95e9b078662589d5ea4c8f5e8d30b
herr-hxp <haxxpet@gmail.com>
2023-08-24 10:25:40 +0200

    test

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<html>
-  <head>
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-    <img src="img/elastx_logo_light.png" />
-      <p><b>Served by:</b> 
-        <?php
-          echo gethostname();
-          echo "<br />" . date("Y-m-d H:i");
-          echo 'we fail'
-        ?>
-    </p>
-  </body>
-</html>
+<?php ekajeka ?>
